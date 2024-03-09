270d13192f981b9816db9661e0bf64ad738b18b0
SomdeepR2002 <134730823+SomdeepR2002@users.noreply.github.com>
2024-03-09 22:02:09 +0530

    modified popup files, some UI work

---
+++ popups/popup.js
@@ -0,0 +1,23 @@
+console.log("This is a popup")
+const websiteData = [
+    {name:"www.randomsite1.com",timeSpent:"3.5 hours"},
+    {name:"www.randomsite2.com", timeSpent:"2 hours"}
+]
+
+function populateWebsiteList(){
+    const websiteList = document.getElementById("website-list")
+    websiteData.forEach(website => {
+        const li = document.createElement("li")
+        li.textContent = `${website.name}: ${website.timeSpent}`;
+        websiteList.appendChild(li);
+    })
+}
+
+function showMoreDetails(){
+    //code to display more details when the button is clicked
+    console.log("Show more details clicked")
+}
+
+populateWebsiteList();
+
+document.getElementById("details-button").addEventListener("click",showMoreDetails)
